4314e0da828824a372e00698fb469170d0584b9f
Agustin c <33077305+agusc01@users.noreply.github.com>
2024-07-24 09:12:23 -0300

    Improving scripts

--- delete_all_baks.sh
+++ delete_all_baks.sh
@@ -1,5 +1,5 @@
-rm -v ~/.bash_aliases.bak &&
-rm -v ~/.bash_profile.bak &&
-rm -v ~/.bashrc.bak &&
-rm -v ~/.gitcustom.sh.bak &&
+rm -v ~/.bash_aliases.bak
+rm -v ~/.bash_profile.bak
+rm -v ~/.bashrc.bak
+rm -v ~/.gitcustom.sh.bak
 rm -v ~/.gitconfig.bak
--- paste_all_settings.sh
+++ paste_all_settings.sh
@@ -1,8 +1,10 @@
-mv -v ~/.bash_aliases{,.bak} &&
-mv -v ~/.bash_profile{,.bak} &&
-mv -v ~/.bashrc{,.bak} &&
-mv -v ~/.gitcustom.sh{,.bak} &&
-mv -v ~/.gitconfig{,.bak} &&
+# backup time !
+mv -v ~/.bash_aliases{,.bak}
+mv -v ~/.bash_profile{,.bak} 
+mv -v ~/.bashrc{,.bak} 
+mv -v ~/.gitcustom.sh{,.bak} 
+mv -v ~/.gitconfig{,.bak} 
 
-cp -v ~/.bash-settings/.bash{_aliases,_profile,rc} ~/ &&
+# paste time !
+cp -v ~/.bash-settings/.bash{_aliases,_profile,rc} ~/ 
 cp -v ~/.bash-settings/.git{config,custom.sh} ~/
